766b77e9dddc25c35dbed080f9f0df2ce17c43f0
OleksandrJS <thelordofthebluff@gmail.com>
2020-06-25 01:36:27 +0300

    Updates

---
+++ precache-manifest.67e5bee1ee83a5342cd4d9edee1a515a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce87017e42e0ba451605abd6f486239",
+    "url": "/react-flights-search/index.html"
+  },
+  {
+    "revision": "7994d3906cfcf21cff46",
+    "url": "/react-flights-search/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6fd7b1528499e45cec2c",
+    "url": "/react-flights-search/static/css/main.7f9c9884.chunk.css"
+  },
+  {
+    "revision": "7994d3906cfcf21cff46",
+    "url": "/react-flights-search/static/js/2.2eb22dea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-flights-search/static/js/2.2eb22dea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd7b1528499e45cec2c",
+    "url": "/react-flights-search/static/js/main.6519b99a.chunk.js"
+  },
+  {
+    "revision": "dec030bd35e96eb5d76c",
+    "url": "/react-flights-search/static/js/runtime-main.95859e8b.js"
+  },
+  {
+    "revision": "dc0320ced577e13e4c9096ea2b8d9bee",
+    "url": "/react-flights-search/static/media/plane.dc0320ce.svg"
+  }
+]);
